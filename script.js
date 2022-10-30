2020345d689444d6bf048054afc989ef6a8f4d29
Gouranga Satapathy <pulusatapathy@gmail.com>
2022-10-30 18:57:30 +0530

    implimented using async await

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let dogImageFit = document.getElementById('doggy');
 let dogImageApi = "https://dog.ceo/api/breeds/image/random";
 
+const dogImageFetch = async () => {
+    try{
+        const response = await fetch(dogImageApi);
+        const responseResult = await response.json();
+        dogImageFit.src = responseResult.message
+        
+    }
+    catch(error){
+        console.log("errrrr")
+        dogImageFit.src = "https://pyxis.nymag.com/v1/imgs/0a8/5d1/3139b0b2fc427ff34fe5394bd1625d8527-2019-critics-joker.rsquare.w700.jpg"
+    }
+}
 
-console.log('twrwq',dogImageFit)
-
-fetch('https://dog.ceo/api/breeds/image/random')
-.then(r=> {
-    console.log("r ",r)
-    return r.json()
-})
-.then(j=>{
-    dogImageFit.src = j.message
-})
+dogImageFetch()
